f0b8bd9daa7076fcd38faa7a6adffa9295da7c5d
toto <test@test.com>
2022-11-04 17:29:24 +0200

    view add tasks show

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,5 +2,37 @@
 @section('content')
     <h2>All tasks</h2>
     <a href="{{ route('task.create') }}">Create new task</a>
-    <!-- TODO: Текущие задачи -->
+    @if (count($tasks) > 0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Tasks
+            </div>
+            <div class="panel-body">
+                <table class="table table-striped task-table">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($tasks as $task)
+                        <tr>
+                            <td class="table-text">
+                                <div>{{ $task->id }}</div>
+                            </td>
+                            <td class="table-text">
+                                <div>{{ $task->name }}</div>
+                            </td>
+                            <td>
+                                <!-- TODO: Кнопка Удалить -->
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
 @endsection
